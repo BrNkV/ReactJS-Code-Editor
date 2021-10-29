af2997fc7be9832c01f7634c50620645cb5e06bd
EveryBarry <burenkov.igor@gmail.com>
2021-10-29 18:55:07 +0300

    navbar + css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { EditorProvider } from './context/context';
+import Codebar from './components/CodeBar/CodeBar';
 
 function App() {
   return (
     <EditorProvider>
-
+      <Codebar/>
     </EditorProvider>
   );
 }
--- src/components/CodeBar/CodeBar.js
+++ src/components/CodeBar/CodeBar.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import styles from './Codebar.module.css';
+import React, { useState } from 'react';
+import styles from './CodeBar.module.css';
 
 const Codebar = () => {
+
+    const [activeTab, setActiveTab] = useState('html');
+
     return (
-        <div>
+        <div className={styles.codebar}>
+            <nav className={styles.tab}>
+                <button
+                    onClick={() => setActiveTab('html')}
+                    className={`${styles.item} ${activeTab === 'html' ? styles.activeTab : ''}`}>
+                    HTML
+                </button>
+
+                <button
+                    onClick={() => setActiveTab('css')}
+                    className={`${styles.item} ${activeTab === 'css' ? styles.activeTab : ''}`}>
+                    CSS
+                </button>
+
+                <button
+                    onClick={() => setActiveTab('js')}
+                    className={`${styles.item} ${activeTab === 'js' ? styles.activeTab : ''}`}>
+                    JS
+                </button>
+            </nav>
+
+            <div className={styles.editor}>
+                {activeTab === 'html' ? <div>HTML</div> : null}
+                {activeTab === 'css' ? <div>CSS</div> : null}
+                {activeTab === 'js' ? <div>JS</div> : null}
+            </div>
 
         </div>
     );
