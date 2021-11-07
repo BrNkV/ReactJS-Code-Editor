9fa78548e174dbcecc859c39c7b7939eb6c95468
EveryBarry <burenkov.igor@gmail.com>
2021-11-07 19:38:48 +0300

    Updates

---
+++ precache-manifest.e2ed5ecd2f392d7ae7ae8a8750478436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866683fbf43f42289cf96ea9367e0d77",
+    "url": "/ReactJS-Code-Editor/index.html"
+  },
+  {
+    "revision": "8f4e5e0c66c2277caae9",
+    "url": "/ReactJS-Code-Editor/static/css/main.fa493d91.chunk.css"
+  },
+  {
+    "revision": "37acd40294a592e7a43e",
+    "url": "/ReactJS-Code-Editor/static/js/2.ffd03aca.chunk.js"
+  },
+  {
+    "revision": "7540180e027fee4aac08f5b74b8a356a",
+    "url": "/ReactJS-Code-Editor/static/js/2.ffd03aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4e5e0c66c2277caae9",
+    "url": "/ReactJS-Code-Editor/static/js/main.182cc2a0.chunk.js"
+  },
+  {
+    "revision": "e468ad5ffe92f09d0c52",
+    "url": "/ReactJS-Code-Editor/static/js/runtime-main.aab13615.js"
+  }
+]);
